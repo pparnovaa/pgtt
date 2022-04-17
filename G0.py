6f04d1e28ab3399c3aa157ae101effaaab8e8f50
pparnovaa <100772416+pparnovaa@users.noreply.github.com>
2022-04-17 17:59:23 +0300

    Update G0.py

--- G0.py
+++ G0.py
@@ -433,403 +433,218 @@ st.sidebar.markdown('**6) Располагаемый теплоперепад с
 H = st.sidebar.number_input('', value=110)
 st.sidebar.write('H_0 = ', H, "кДж/кг")
 
-
-
-
-
-
-D = list(range(value_d[0],value_d[1],1))
-d = [t * 10**-2 for t in D]
-p_0 = p0
-T_0 = t0+273.15
-n = 60
-G_0 = 698.7025
-H_0 = H
-rho = 0.05
-l_1 = 0.015
-alpha_1 = 12
-b_1 = 0.06
-Delta = 0.003
-b_2 = 0.03
-kappa_vs = 0
-
-
-
-def callculate_optimum(d, p_0, T_0, n, G_0, H_0, rho, l_1, alpha_1, b_1, Delta, b_2, kappa_vs):
-    u = M.pi * d * n
-    point_0 = WSP(P=p_0, T=T_0)
-    H_0s = H_0 * (1 - rho)
-    H_0r = H_0 * rho
-    h_1t = point_0.h - H_0s
-    point_1t = WSP(h=h_1t, s=point_0.s)
-    c_1t = (2000 * H_0s) ** 0.5
-    M_1t = c_1t / point_1t.w
-    mu_1 = 0.982 - 0.005 * (b_1 / l_1)
-    F_1 = G_0 * point_1t.v / mu_1 / c_1t
-    el_1 = F_1 / M.pi / d / M.sin(M.radians(alpha_1))
-    e_opt = 5 * el_1 ** 0.5
-    if e_opt > 0.85:
-        e_opt = 0.85
-    l_1 = el_1 / e_opt
-
-    fi_1 = 0.98 - 0.008 * (b_1 / l_1)
-    c_1 = c_1t * fi_1
-    alpha_1 = M.degrees(M.asin(mu_1 / fi_1 * M.sin(M.radians(alpha_1))))
-    w_1 = (c_1 ** 2 + u ** 2 - 2 * c_1 * u * M.cos(M.radians(alpha_1))) ** 0.5
-    betta_1 = M.degrees(M.atan(M.sin(M.radians(alpha_1)) / (M.cos(M.radians(alpha_1)) - u / c_1)))
-    Delta_Hs = c_1t ** 2 / 2 * (1 - fi_1 ** 2)
-    h_1 = h_1t + Delta_Hs * 1e-3
-    point_1 = WSP(P=point_1t.P, h=h_1)
-    h_2t = h_1 - H_0r
-    point_2t = WSP(h=h_2t, s=point_1.s)
-    w_2t = (2 * H_0r * 1e3 + w_1 ** 2) ** 0.5
-    l_2 = l_1 + Delta
-    mu_2 = 0.965 - 0.01 * (b_2 / l_2)
-    M_2t = w_2t / point_2t.w
-    F_2 = G_0 * point_2t.v / mu_2 / w_2t
-    betta_2 = M.degrees(M.asin(F_2 / (e_opt * M.pi * d * l_2)))
-    point_1w = WSP(h=point_1.h + w_1 ** 2 / 2 * 1e-3, s=point_1.s)
-
-    psi = 0.96 - 0.014 * (b_2 / l_2)
-    w_2 = psi * w_2t
-    c_2 = (w_2 ** 2 + u ** 2 - 2 * u * w_2 * M.cos(M.radians(betta_2))) ** 0.5
-    alpha_2 = M.degrees(M.atan(M.sin(M.radians(betta_2)) / (M.cos(M.radians(betta_2)) - u / w_2)))
-    if alpha_2 < 0:
-        alpha_2 = 180 + alpha_2
-    Delta_Hr = w_2t ** 2 / 2 * (1 - psi ** 2)
-    h_2 = h_2t + Delta_Hr * 1e-3
-    point_2 = WSP(P=point_2t.P, h=h_2)
-    Delta_Hvs = c_2 ** 2 / 2
-    E_0 = H_0 - kappa_vs * Delta_Hvs
-    etta_ol1 = (E_0 * 1e3 - Delta_Hs - Delta_Hr - (1 - kappa_vs) * Delta_Hvs) / (E_0 * 1e3)
-    etta_ol2 = (u * (c_1 * M.cos(M.radians(alpha_1)) + c_2 * M.cos(M.radians(alpha_2)))) / (E_0 * 1e3)
-    return etta_ol2, alpha_2
-
-st.write(' Зависимость КПД')
-
-
-
-H_0 = [i for i in list(range(90,110,1))]
-alpha1 = []
-eta = []
-ucf = []
-for i in H_0:
-    ucf_1 = M.pi*d*n/(2000*i)**0.5
-    ucf.append(ucf_1)
-
-    eta_ol, alpha = callculate_optimum(d, p_0, T_0, n, G_0, i, rho, l_1, alpha_1, b_1, Delta, b_2, kappa_vs)
-    eta.append(eta_ol)
-    alpha1.append(alpha)
-
-
-d = 1.1
-u = M.pi * d * n
-point_0 = WSP(P=p_0, T=T_0)
-H_0s = H_0 * (1 - rho)
-H_0r = H_0 * rho
-h_1t = point_0.h - H_0s
-point_1t = WSP(h=h_1t, s=point_0.s)
-c_1t = (2000 * H_0s) ** 0.5
-M_1t = c_1t / point_1t.w
-mu_1 = 0.982 - 0.005 * (b_1 / l_1)
-F_1 = G_0 * point_1t.v / mu_1 / c_1t
-el_1 = F_1 / M.pi / d / M.sin(M.radians(alpha_1))
-e_opt = 6 * el_1 ** 0.5
-if e_opt > 0.85:
-    e_opt = 0.85
-l_1 = el_1 / e_opt
-
-
-
-
-def plot_hs_nozzle_t(x_lim, y_lim):
-    plt.plot([point_0.s, point_1t.s], [point_0.h, point_1t.h], 'ro-')
-    iso_bar(point_0, -0.02, 0.02, 0.001, 'y')
-    iso_bar(point_1t, -0.02, 0.02, 0.001, 'y')
-    plt.xlim(x_lim)
-    plt.ylim(y_lim)
-
-
-plot_hs_nozzle_t([6.1, 6.25], [3200, 3500])
-
-
-
-
-
-
-if alpha_1 <= 10:
-    NozzleBlade = 'C-90-09A'
-    t1_ = 0.78
-    b1_mod = 6.06
-    f1_mod = 3.45
-    W1_mod = 0.471
-    alpha_inst1 = alpha_1 - 12.5 * (t1_ - 0.75) + 20.2
-elif 10 < alpha_1 <= 13:
-    NozzleBlade = 'C-90-12A'
-    t1_ = 0.78
-    b1_mod = 5.25
-    f1_mod = 4.09
-    W1_mod = 0.575
-    alpha_inst1 = alpha_1 - 10 * (t1_ - 0.75) + 21.2
-elif 13 < alpha_1 <= 16:
-    NozzleBlade = 'C-90-15A'
-    t1_ = 0.78
-    b1_mod = 5.15
-    f1_mod = 3.3
-    W1_mod = 0.45
-    alpha_inst1 = alpha_1 - 16 * (t1_ - 0.75) + 23.1
-else:
-    NozzleBlade = 'C-90-18A'
-    t1_ = 0.75
-    b1_mod = 4.71
-    f1_mod = 2.72
-    W1_mod = 0.333
-    alpha_inst1 = alpha_1 - 17.7 * (t1_ - 0.75) + 24.2
-
-z1 = (M.pi * d * 0.85) / (b_1 * t1_)
-z1 = int(z1)
-
-t1_ = (M.pi * d * 0.85) / (b_1 * z1)
-Ksi_1_ = (0.021042 * b_1 / l_1 + 0.023345) * 100
-k_11 = 7.18977510 * M_1t ** 5 - 26.94497258 * M_1t ** 4 + 39.35681781 * M_1t ** 3 - 26.09044664 * M_1t ** 2 + 6.75424811 * M_1t + 0.69896998
-k_12 = 0.00014166 * 90 ** 2 - 0.03022881 * 90 + 2.61549380
-k_13 = 13.25474043 * t1_ ** 2 - 20.75439502 * t1_ + 9.12762245
-Ksi_1 = Ksi_1_ * k_11 * k_12 * k_13
-
-fi_1 = M.sqrt(1 - Ksi_1 / 100)
-
-
-
-
-alpha_1 = 12
-c_1 = c_1t * fi_1
-
-alpha_1 = M.degrees(M.asin(mu_1 / fi_1 * M.sin(M.radians(alpha_1))))
-
-w_1 = (c_1 ** 2 + u ** 2 - 2 * c_1 * u * M.cos(M.radians(alpha_1))) ** 0.5
-
-
-c_1u = c_1 * M.cos(M.radians(alpha_1))
-c_1a = c_1 * M.sin(M.radians(alpha_1))
-w_1u = c_1u - u
-
-w_1_tr = [0, 0, -w_1u, -c_1a]
-c_1_tr = [0, 0, -c_1u, -c_1a]
-u_1_tr = [-w_1u, -c_1a, -u, 0]
-
-
-
-betta_1 = M.degrees(M.atan(M.sin(M.radians(alpha_1)) / (M.cos(M.radians(alpha_1)) - u / c_1)))
-
-Delta_Hs = c_1t ** 2 / 2 * (1 - fi_1 ** 2)
-
-
-
-h_1 = h_1t + Delta_Hs * 1e-3
-
-point_1 = WSP(P=point_1t.P, h=h_1)
-h_2t = h_1 - H_0r
-
-point_2t = WSP(h=h_2t, s=point_1.s)
-w_2t = (2 * H_0r * 1e3 + w_1 ** 2) ** 0.5
-
-l_2 = l_1 + Delta
-mu_2 = 0.965 - 0.01 * (b_2 / l_2)
-
-M_2t = w_2t / point_2t.w
-
-F_2 = G_0 * point_2t.v / mu_2 / w_2t
-
-betta_2 = M.degrees(M.asin(F_2 / (e_opt * M.pi * d * l_2)))
-
-point_1w = WSP(h=point_1.h + w_1 ** 2 / 2 * 1e-3, s=point_1.s)
-
-
-
-if betta_2 <= 15:
-    RotorBlade = 'P-23-14A'
-    t2_ = 0.63
-    b2_mod = 2.59
-    f2_mod = 2.44
-    W2_mod = 0.39
-    beta_inst2 = betta_2 - 12.5 * (t2_ - 0.75) + 20.2
-
-elif 15 < betta_2 <= 19:
-    RotorBlade = 'P-26-17A'
-    t2_ = 0.65
-    b2_mod = 2.57
-    f2_mod = 2.07
-    W2_mod = 0.225
-    beta_inst2 = betta_2 - 19.3 * (t2_ - 0.6) + 60
-
-
-elif 19 < betta_2 <= 23:
-    RotorBlade = 'P-30-21A'
-    t2_ = 0.63
-    b2_mod = 2.56
-    f2_mod = 1.85
-    W2_mod = 0.234
-    beta_inst2 = betta_2 - 12.8 * (t2_ - 0.65) + 58
-
-
-elif 23 < betta_2 <= 27:
-    RotorBlade = 'P-35-25A'
-    t2_ = 0.6
-    b2_mod = 2.54
-    f2_mod = 1.62
-    W2_mod = 0.168
-    beta_inst2 = betta_2 - 16.6 * (t2_ - 0.65) + 54.3
-
-elif 27 < betta_2 <= 31:
-    RotorBlade = 'P-46-29A'
-    t2_ = 0.51
-    b2_mod = 2.56
-    f2_mod = 1.22
-    W2_mod = 0.112
-    beta_inst2 = betta_2 - 50.5 * (t2_ - 0.6) + 47.1
-
-
-else:
-    RotorBlade = 'P-50-33A'
-    t2_ = 0.49
-    b2_mod = 2.56
-    f2_mod = 1.02
-    W2_mod = 0.079
-    beta_inst2 = betta_2 - 20.8 * (t2_ - 0.6) + 43.7
-
-
-
-z2 = int((M.pi * d) / (b_2 * t2_))
-t2_ = (M.pi * d) / (b_2 * z2)
-Ksi_2_ = 4.364 * b_2 / l_2 + 4.22
-k_21 = -13.79438991 * M_2t ** 4 + 36.69102267 * M_2t ** 3 - 32.78234341 * M_2t ** 2 + 10.61998662 * M_2t + 0.28528786
-k_22 = 0.00331504 * betta_1 ** 2 - 0.21323910 * betta_1 + 4.43127194
-k_23 = 60.72813684 * t2_ ** 2 - 76.38053189 * t2_ + 24.97876023
-Ksi_2 = Ksi_2_ * k_21 * k_22 * k_23
-
-psi = M.sqrt(1 - Ksi_2 / 100)
-
-
-
-psi = 0.93
-
-w_2 = psi * w_2t
-
-c_2 = (w_2 ** 2 + u ** 2 - 2 * u * w_2 * M.cos(M.radians(betta_2))) ** 0.5
-
-alpha_2 = M.degrees(M.atan(M.sin(M.radians(betta_2)) / (M.cos(M.radians(betta_2)) - u / w_2)))
-
-Delta_Hr = w_2t ** 2 / 2 * (1 - psi ** 2)
-
-h_2 = h_2t + Delta_Hr * 1e-3
-point_2 = WSP(P=point_2t.P, h=h_2)
-Delta_Hvs = c_2 ** 2 / 2
-
-E_0 = H_0 - kappa_vs * Delta_Hvs
-etta_ol1 = (E_0 * 1e3 - Delta_Hs - Delta_Hr - (1 - kappa_vs) * Delta_Hvs) / (E_0 * 1e3)
-
-etta_ol2 = (u * (c_1 * M.cos(M.radians(alpha_1)) + c_2 * M.cos(M.radians(alpha_2)))) / (E_0 * 1e3)
-
-
-
-
-c_1u = c_1 * M.cos(M.radians(alpha_1))
-c_1a = c_1 * M.sin(M.radians(alpha_1))
-w_1u = c_1u - u
-w_2a = w_2 * M.sin(M.radians(betta_2))
-w_2u = w_2 * M.cos(M.radians(betta_2))
-c_2u = w_2u - u
-
-w_1_tr = [0, 0, -w_1u, -c_1a]
-c_1_tr = [0, 0, -c_1u, -c_1a]
-u_1_tr = [-w_1u, -c_1a, -u, 0]
-
-w_2_tr = [0, 0, w_2u, -w_2a]
-c_2_tr = [0, 0, c_2u, -w_2a]
-u_2_tr = [w_2u, -w_2a, -u, 0]
-
-
-Gr5 = plt.figure()
-ax = plt.axes()
-ax.arrow(*c_1_tr, head_width=5, length_includes_head=True, head_length=20, fc='r', ec='r')
-ax.arrow(*w_1_tr, head_width=5, length_includes_head=True, head_length=20, fc='b', ec='b')
-ax.arrow(*u_1_tr, head_width=5, length_includes_head=True, head_length=20, fc='g', ec='g')
-ax.arrow(*c_2_tr, head_width=5, length_includes_head=True, head_length=20, fc='r', ec='r')
-ax.arrow(*w_2_tr, head_width=5, length_includes_head=True, head_length=20, fc='b', ec='b')
-ax.arrow(*u_2_tr, head_width=5, length_includes_head=True, head_length=20, fc='g', ec='g')
-plt.text(-2 * c_1u / 3, -3 * c_1a / 4, '$c_1$', fontsize=20)
-plt.text(-2 * w_1u / 3, -3 * c_1a / 4, '$w_1$', fontsize=20)
-plt.text(2 * c_2u / 3, -3 * w_2a / 4, '$c_2$', fontsize=20)
-plt.text(2 * w_2u / 3, -3 * w_2a / 4, '$w_2$', fontsize=20)
+def iso_bar(wsp_point, min_s=-0.1, max_s=0.11, step_s=0.011, color = 'b'):
+    if not isinstance(wsp_point,list):
+        iso_bar_0_s = np.arange(wsp_point.s+min_s,wsp_point.s+max_s,step_s).tolist()
+        iso_bar_0_h = [IAPWS97(P = wsp_point.P, s = i).h for i in iso_bar_0_s]
+    else:
+        iso_bar_0_s = np.arange(wsp_point[0].s+min_s,wsp_point[1].s+max_s,step_s).tolist()
+        iso_bar_0_h = [IAPWS97(P = wsp_point[1].P, s = i).h for i in iso_bar_0_s]
+    plt.plot(iso_bar_0_s,iso_bar_0_h,color)
+    
+d = 1.1 #m
+p_0 = 28 #МПа
+t_0 = 570 #град Цельсия
+T_0 = t_0+273.15 #K
+n = 60 #Гц
+G_0 = 443.936265 #кг/с
+G_k = 292.733965 #кг/с
+H_0 = 100 #кДж/кг
+rho = 0.05 #степень реактивности
+l_1 = 0.015 #м
+alpha_1 = 12 #град
+b_1 = 0.06 #м
+Delta = 0.003 #м
+b_2 = 0.03 #м
+kappa_vs = 0 #коэффициент использования выходной скорости
+
+
+D1 = drs - deltaD
+sat_steam = IAPWS97(P=P0, h=h0)
+s_0 = sat_steam.s
+t_0 = sat_steam.T
+error = 2
+i = 1
+while error > 0.5:
+    rho = rho_s + 1.8 / (tetta + 1.8)
+    X = (fi * M.cos(M.radians(alfa))) / (2 * M.sqrt(1 - rho))
+    H01 = 12.3 * (D1 / X) ** 2 * (n / 50) ** 2
+    h2t = h0 - H01
+    steam2t = IAPWS97(h=h2t, s=s_0)
+    v2t = steam2t.v
+    l11 = G0 * v2t * X / (M.pi ** 2 * D1 ** 2 * n * M.sqrt(1 - rho) * M.sin(M.radians(alfa)) * mu1)
+    tetta_old = tetta
+    tetta = D1 / l11
+    error = abs(tetta - tetta_old) / tetta_old * 100
+    i += 1
+
+l21 = l11 + delta
+d_s = D1 - l21
+steam_tz = IAPWS97(P=Pz, s=s_0)
+h_zt = steam_tz.h
+H0 = h0 - h_zt
+Hi = H0 * etaoi
+h_z = h0 - Hi
+steam_z = IAPWS97(P=Pz, h=h_z)
+v_2z = steam_z.v
+x = Symbol('x')
+с = solve(x ** 2 + x * d_s - (l21 * (d_s + l21) * v_2z / v2t))
+for j in с:
+    if j > 0:
+        l2z = j
+d2z = d_s + l2z
+tetta1 = (l21 + d_s) / l21
+tettaz = (l2z + d_s) / l2z
+rho1 = rho_s + 1.8 / (1.8 + tetta1)
+rhoz = rho_s + 1.8 / (1.8 + tettaz)
+X1 = (fi * M.cos(M.radians(alfa))) / (2 * M.sqrt(1 - rho1))
+Xz = (fi * M.cos(M.radians(alfa))) / (2 * M.sqrt(1 - rhoz))
+
+DeltaZ = 1
+ite = 0
+while DeltaZ > 0:
+    matr = []
+    Num = 0
+    SumH = 0
+    for _ in range(int(Z)):
+        li = (l21 - l2z) / (1 - Z) * Num + l21
+        di = (D1 - d2z) / (1 - Z) * Num + D1
+        tetta_i = di / li
+        rho_i = rho_s + 1.8 / (1.8 + tetta_i)
+        X_i = (fi * M.cos(M.radians(alfa))) / (2 * M.sqrt(1 - rho_i))
+        if Num < 1:
+            H_i = 12.3 * (di / X_i) ** 2 * (n / 50) ** 2
+        else:
+            H_i = 12.3 * (di / X_i) ** 2 * (n / 50) ** 2 * 0.95
+        Num = Num + 1
+        H_d = 0
+        SumH = SumH + H_i
+        matr.append([Num, round(di, 3), round(li, 3), round(tetta_i, 2), round(rho_i, 3), round(X_i, 3), round(H_i, 2),
+                     round(H_d, 2)])
+    H_m = SumH / Z
+    q_t = 4.8 * 10 ** (-4) * (1 - etaoi) * H0 * (Z - 1) / Z
+    Z_new = round(H0 * (1 + q_t) / H_m)
+    DeltaZ = abs(Z - Z_new)
+    # print(ite, Z)
+    Z = Z_new
+    ite += 1
+DeltaH = (H0 * (1 + q_t) - SumH) / Z
+a = 0
+for elem in matr:
+    matr[a][7] = round(elem[6] + DeltaH, 2)
+    a += 1
+
+N_ = []
+di_ = []
+li_ = []
+tettai_ = []
+rhoi_ = []
+Xi_ = []
+Hi_ = []
+Hdi_ = []
+a = 0
+for elem in matr:
+    N_.append(matr[a][0])
+    di_.append(matr[a][1])
+    li_.append(matr[a][2])
+    tettai_.append(matr[a][3])
+    rhoi_.append(matr[a][4])
+    Xi_.append(matr[a][5])
+    Hi_.append(matr[a][6])
+    Hdi_.append(matr[a][7])
+    a += 1
+
+di_ = [float(x) for x in di_]
+li_ = [float(x) for x in li_]
+tettai_ = [float(x) for x in tettai_]
+rhoi_ = [float(x) for x in rhoi_]
+Xi_ = [float(x) for x in Xi_]
+Hi_ = [float(x) for x in Hi_]
+Hdi_ = [float(x) for x in Hdi_]
+
+## Таблица
+table = pd.DataFrame({"№ ступени": (N_),
+                      "di, м": (di_),
+                      "li, м": (li_),
+                      "θi ": (tettai_),
+                      "ρi ": (rhoi_),
+                      "Xi ": (Xi_),
+                      "Hi, кДж/кг": (Hi_),
+                      "Hi + Δ, кДж/кг": (Hdi_)
+                      }
+                     )
+
+st.dataframe(table)
+
+## Графики
+z = []
+for a in range(1, Z + 1):
+    z.append(a)
+
+st.write("#")
+fig = plt.figure(figsize=(10, 5))
+ax = fig.gca()
+ax.set_xticks(np.arange(1, 15, 1))
 plt.grid(True)
-st.pyplot(Gr5)
+plt.plot(z, di_, '-ro')
+plt.title('Рисунок 1. Распределение средних диаметров по проточной части')
+st.pyplot(fig)
 
+st.write("#")
+fig = plt.figure(figsize=(10, 5))
+ax = fig.gca()
+ax.set_xticks(np.arange(1, 15, 1))
+plt.grid(True)
+plt.plot(z, li_, '-ro')
+plt.title('Рисунок 2. Распределение высот лопаток по проточной части')
+st.pyplot(fig)
 
-delta_a = 0.0025
-z_per_up = 2
-mu_a = 0.5
-mu_r = 0.75
-d_per = d + l_2
-delta_r = d_per * 0.001
-delta_ekv = 1 / M.sqrt(1 / (mu_a * delta_a) ** 2 + z_per_up / (mu_r * delta_r) ** 2)
+st.write("#")
+fig = plt.figure(figsize=(10, 5))
+ax = fig.gca()
+ax.set_xticks(np.arange(1, 15, 1))
+plt.grid(True)
+plt.plot(z, tettai_, '-ro')
+plt.title('Рисунок 3. Распределение обратной веерности по проточной части')
+st.pyplot(fig)
 
-xi_u_b = M.pi * d_per * delta_ekv * etta_ol1 / F_1 * M.sqrt(rho + 1.8 * l_2 / d)
+st.write("#")
+fig = plt.figure(figsize=(10, 5))
+ax = fig.gca()
+ax.set_xticks(np.arange(1, 15, 1))
+plt.grid(True)
+plt.plot(z, rhoi_, '-ro')
+plt.title('Рисунок 4. Распределение степени реактивности по проточной части')
+st.pyplot(fig)
 
-Delta_Hub = xi_u_b * E_0
+st.write("#")
+fig = plt.figure(figsize=(10, 5))
+ax = fig.gca()
+ax.set_xticks(np.arange(1, 15, 1))
+plt.grid(True)
+plt.plot(z, Xi_, '-ro')
+plt.title('Рисунок 5. Распределение U/Cф по проточной части')
+st.pyplot(fig)
 
+st.write("#")
+fig = plt.figure(figsize=(10, 5))
+ax = fig.gca()
+ax.set_xticks(np.arange(1, 15, 1))
+plt.grid(True)
+plt.plot(z, Hi_, '-ro')
+plt.title('Рисунок 6. Распределение теплоперепадов по проточной части')
+st.pyplot(fig)
 
-
-k_tr = 0.0007
-Kappa_VS = 0
-u = M.pi * d * n
-c_f = M.sqrt(2000 * H_0)
-ucf = u / c_f
-xi_tr = k_tr * d ** 2 / F_1 * ucf ** 3
-Delta_Htr = xi_tr * E_0
-
-
-
-k_v = 0.065
-m = 1
-xi_v = k_v / M.sin(M.radians(alpha_1)) * (1 - e_opt) / e_opt * ucf ** 3 * m
-i_p = 4
-B_2 = b_2 * M.sin(M.radians(beta_inst2))
-xi_segm = 0.25 * B_2 * l_2 / F_1 * ucf * etta_ol1 * i_p
-xi_parc = xi_v + xi_segm
-Delta_H_parc = E_0 * xi_parc
-
-
-
-H_i = E_0 - Delta_Hr * 1e-3 - Delta_Hs * 1e-3 - (1 - Kappa_VS) * Delta_Hvs * 1e-3 - Delta_Hub - Delta_Htr - Delta_H_parc
-
-h_3 = h_2 + Delta_Hvs * 1e-3
-point_3 = WSP(P=point_2t.P, h=h_3)
-
-point_2_ = WSP(P=point_2t.P, h=point_0.h-H_0)
-
-Gr4 = plt.figure()
-def plot_hs_stage_t(x_lim, y_lim):
-    plot_hs_nozzle_t(x_lim, y_lim)
-    plt.plot([point_0.s, point_1.s], [point_0.h, point_1.h], 'bo-')
-    plt.plot([point_1.s, point_2t.s], [point_1.h, point_2t.h], 'ro-')
-    plt.plot([point_0.s, point_2_.s], [point_0.h, point_2_.h], 'ro-')
-    plt.plot([point_1.s, point_2.s], [point_1.h, point_2.h], 'bo-')
-    plt.plot([point_2.s, point_3.s], [point_2.h, point_3.h], 'bo-')
-
-    iso_bar(point_2t, -0.02, 0.02, 0.001, 'y')
-    plt.ylabel('H кДж/кг')
-    plt.xlabel('S кДж/кг*К')
-    plt.grid(True)
-
-plot_hs_stage_t([6.1, 6.25], [3200, 3400])
-st.pyplot(Gr4)
+st.write("#")
+fig = plt.figure(figsize=(10, 5))
+ax = fig.gca()
+ax.set_xticks(np.arange(1, 15, 1))
+plt.grid(True)
+plt.plot(z, Hdi_, '-ro')
+plt.title('Рисунок 7. Распределение теплоперепадов с учетом невязки по проточной части')
+st.pyplot(fig)
 
 
 
-eta_oi = H_i / E_0
-st.write("""Внутренний относительный КПД ступени  
-        eta_oi  = %.3f """ % eta_oi)
-N_i = G_0 * H_i
-st.write("""Внутреняя мощность ступени  
-            N_i =  %.2f кВт""" % N_i)
+
 
